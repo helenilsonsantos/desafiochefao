dc61c9ff17da3eb7f4adfe7be727b00b252c17bd
Laura Neves <lauratneves@gmail.com>
2022-06-29 18:13:11 -0300

    refactor: ajustando index de validation de pacientes

--- backend/src/domain/pacientes/validations/index.js
+++ backend/src/domain/pacientes/validations/index.js
@@ -1,11 +1,11 @@
 const validacaoCadastrar =  require('./cadastrar')
 const validacaoListar = require('./listar')
 const validacaoAtualizar = require('./atualizar')
-const validacaoDeletar = require('./desativar')
+const validacaoDesativar = require('./desativar')
 
 module.exports = {
     validacaoCadastrar,
     validacaoListar,
     validacaoAtualizar,
-    validacaoDeletar
+    validacaoDesativar
 }
