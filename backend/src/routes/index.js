bb512eb87cd77a05481979245f0d87cb1691fd42
Pedro Oliveira <pedro_oliva@outlook.com.br>
2022-07-04 21:44:54 -0300

    refactor: mudano rota para o metodo patch

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,25 +4,9 @@ const empresaValidate = require('../domain/empresas/validations/index');
 
 const routes = express.Router();
 
-routes.post(
-  '/empresa',
-  empresaValidate.validarCadastrar,
-  empresasController.cadastrar
-);
-routes.get(
-  '/empresa/:id',
-  empresaValidate.validarListar,
-  empresasController.listar
-);
-routes.put(
-  '/empresa/:id',
-  empresaValidate.validarAtualizar,
-  empresasController.atualizar
-);
-routes.put(
-  '/empresa/desativar/:id',
-  empresaValidate.validarDesativar,
-  empresasController.desativar
-);
+routes.post('/empresa', empresaValidate.validarCadastrar, empresasController.cadastrar);
+routes.get('/empresa/:id', empresaValidate.validarListar, empresasController.listar);
+routes.patch('/empresa/:id', empresaValidate.validarAtualizar, empresasController.atualizar);
+routes.patch('/empresa/desativar/:id', empresaValidate.validarDesativar, empresasController.desativar);
 
 module.exports = routes;
