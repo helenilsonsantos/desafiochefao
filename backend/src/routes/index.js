2466f0e45a68342a7d8690d79e6baf58ad4c7099
Pedro Oliveira <pedro_oliva@outlook.com.br>
2022-07-04 23:39:37 -0300

    fix: rotas

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const empresasController = require('../domain/empresas/controller/empresasController');
 const empresaValidate = require('../domain/empresas/validations/index');
 
-const routes = express.Router();
+const rotas = express.Router();
 
-routes.post('/empresa', empresaValidate.validarCadastrar, empresasController.cadastrar);
-routes.get('/empresa/:id', empresaValidate.validarListar, empresasController.listar);
-routes.patch('/empresa/:id', empresaValidate.validarAtualizar, empresasController.atualizar);
-routes.patch('/empresa/desativar/:id', empresaValidate.validarDesativar, empresasController.desativar);
+rotas.post('/empresa', empresaValidate.validarCadastrar, empresasController.cadastrar);
+rotas.get('/empresa/:id', empresaValidate.validarListar, empresasController.listar);
+rotas.patch('/empresa/:id', empresaValidate.validarAtualizar, empresasController.atualizar);
+rotas.patch('/empresa/desativar/:id', empresaValidate.validarDesativar, empresasController.desativar);
 
-module.exports = routes;
+module.exports = rotas;
