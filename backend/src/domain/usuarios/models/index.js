64d5bdf56bee9bdc29253df58d373274728af5b1
Helenilson Holamek <helenilson@meuhg.com>
2022-07-05 21:24:51 -0300

    refactor: ajustes no código

--- backend/src/domain/usuarios/models/index.js
+++ backend/src/domain/usuarios/models/index.js
@@ -1,11 +1,4 @@
 const Usuarios = require ('../models/Usuarios')
-const Empresas = require ('../../empresas/models/Empresas')
-
-Usuarios.belongsTo (
-    Empresas, {
-        foreignkey: 'empresa_id'
-    }
-)
 
 module.exports = {
     Usuarios
