880aab7fc10eb543605874274d49ac7b50524c84
Gabriela de Valnisio <gabi.valnisio23@gmail.com>
2022-07-07 23:25:54 -0300

    Atalizando a página de cadastro

--- frontend/src/Pages/Cadastro/index.jsx
+++ frontend/src/Pages/Cadastro/index.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, Button } from "react-bootstrap";
 import fotoCadastro from "../../Assets/foto-cadastro.png";
+import setaVoltar from "../../Assets/seta-esquerda.png";
 import FormCadastro from "../../Components/FormCadastro";
 import "./styles.css";
 
 const Cadastro = () => {
 	return (
-		<div>
+		<div className="container-pagina-cadastro">
 			{/* Header */}
 
 			<Container id="container-cadastro">
 				<Card id="card-foto">
+					<Button>
+						<img src={setaVoltar} alt="ícone de voltar" />
+						Voltar
+					</Button>
 					<img src={fotoCadastro} alt="foto da página de cadastro" id="foto" />
 				</Card>
-				<Card id="card-form-cadastro">
-					<div>
-						<h3>Comece agora</h3>
-						<h4>Organize sua clínica</h4>
-					</div>
 
+				<Card id="card-form-cadastro">
+					<h3>Preencha seus dados</h3>
 					<FormCadastro />
 				</Card>
 			</Container>
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -4,12 +4,16 @@ import {
 	BrowserRouter,
 } from "react-router-dom";
 import Cadastro from "./Pages/Cadastro";
+import Home from "./Pages/Home";
 
 export default function Routes() {
 	return (
 		<BrowserRouter>
 			<WrapperRoutes>
-				<Route path="/" element={<Cadastro />} />
+				<Route path="/cadastro" element={<Cadastro />} />
+				<Route path="/" element={<Home />} />
+
+
 			</WrapperRoutes>
 		</BrowserRouter>
 	);
