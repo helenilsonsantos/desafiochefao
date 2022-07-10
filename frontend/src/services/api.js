1c97514998f7f99f8c658941d47e23990a991e87
MicaelLDO <90872990+MicaelLDO@users.noreply.github.com>
2022-07-10 16:08:58 -0300

    fix: correção do link da api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 
 const Api = axios.create({
-    baseURL: process.env.API_URL || 'http://localhost:4350'
+    baseURL: process.env.API_URL || 'https://techdental.herokuapp.com'
 });
 
-Api.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsInBlcmZpbCI6ImFkbWluaXN0cmFkb3IiLCJzaXR1YWNhbyI6ImF0aXZvIiwiZW1wcmVzYV9pZCI6MTEsImlhdCI6MTY1NzQ3NjY5OX0.ufW7xCICL3OwrnPFWcV3kHhYs3x4o2XIE1B-8wLEWmk'
+Api.defaults.headers.common['Authorization'] = ""
 
 export { Api }
