05c275f8104360b0fa020a8461ca7043ea2764c1
João Menezes <joaosmenezes@hotmail.com>
2022-07-10 23:40:45 -0300

    alteração nas rotas e inicio da integração login

--- frontend/src/Pages/InfoPaciente/index.jsx
+++ frontend/src/Pages/InfoPaciente/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import ModalNovoUser from '../../Componentes/ModalNovoUser';
 import '../../Componentes/Container/styles.css'
 import HeaderLogado from '../../Componentes/HeaderLogado';
 import InfoPaciente from '../../Componentes/InfoPaciente';
-import ModalEditUser from '../../Componentes/ModalEditUser';
 import Sidebar from '../../Componentes/MenuLateral/Sidebar';
 
 const Paciente= () => {
