0876117a94498b6bc9a85c77a4e27a570ea2b5ca
Gabriela de Valnisio <gabi.valnisio23@gmail.com>
2022-07-11 07:58:40 -0300

    Tornando páginas responsivas

--- frontend/src/Pages/ConfigUser/index.jsx
+++ frontend/src/Pages/ConfigUser/index.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import ConfigUser from "../../Componentes/ConfigUser/ConfigUser";
+import InfoConta from "../../Componentes/InfoConta";
+import InfoPlano from "../../Componentes/InfoPlano";
 import HeaderLogado from "../../Componentes/HeaderLogado";
 import Sidebar from "../../Componentes/MenuLateral/Sidebar";
-import "./styles.css"
+import "./styles.css";
+import { Container } from "react-bootstrap";
 
 function ConfigUserPage() {
 	return (
 		<div className="config-pagina">
 			<HeaderLogado />
 			<Sidebar />
-			<ConfigUser />
+			<Container id="config-adm">
+				<InfoConta
+					nome="Beatriz Neves"
+					email="beatriz.neves@bol.com.br"
+					cnpj="XX. XXX. XXX/0001-XX"
+				/>
+
+				<hr />
+
+				<InfoPlano
+					nomePlano="Plano anual Dental Platinum PRO"
+					infoPlano="Consultar informações sobre meu plano"
+					idConsultorio="xxxxxxx"
+				/>
+			</Container>
 		</div>
 	);
 }
