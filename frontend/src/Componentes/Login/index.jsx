94f3360ae4c09fc96e3991093bb4426408582694
Gabriela de Valnisio <gabi.valnisio23@gmail.com>
2022-07-12 18:20:33 -0300

    Alterações na página de Login

--- frontend/src/Componentes/Login/index.jsx
+++ frontend/src/Componentes/Login/index.jsx
@@ -3,42 +3,91 @@ import EmailLogin from "../../Assets/mail.svg";
 import Lock from "../../Assets/lock.svg";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Alert } from "react-bootstrap";
 import "./style.css";
-import {useFormik} from "formik";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import user from "../../Store/modules/user";
-import {login} from "../../services/login"
+import { login } from "../../services/login";
+
+const validationSchema = Yup.object({
+	email: Yup.string()
+		.email("Utilize um email válido")
+		.required("Insira seu email"),
+	senha: Yup.string()
+		.required("Insira sua senha")
+		.min(8, "Sua senha deve ter no mínimo 8 caracteres")
+		.max(12, "Sua senha deve ter no máximo 12 caracteres"),
+});
 
 function Login() {
+	const alerta = () => {
+		return (
+			<Alert variant="danger">
+				E-mail ou senha inseridos não cadastrado, tente novamente!
+			</Alert>
+		);
+	};
+
 	const navigate = useNavigate();
-	const formik = useFormik({
-		initialValues: {
-			email: "",
-			senha: ""
-		},
-		onSubmit: async values => {
-			const response = await login(values)
-			console.log(response)
-		}
-	})
 	const navigateToCadastro = () => {
 		navigate("/cadastro");
 	};
 
+	const formik = useFormik({
+		initialValues: {
+			email: "",
+			senha: "",
+		},
+		validationSchema,
+		onSubmit: async (values, { resetForm }) => {
+			const response = await login(values);
+			console.log(response);
+			resetForm({ values: "" });
+		},
+	});
+
 	return (
 		<section className="sessao-login">
-			<img className="sessao-login-img" src={ConsultorioLogin} alt="imagem da página de login" />
+			<img
+				className="sessao-login-img"
+				src={ConsultorioLogin}
+				alt="imagem da página de login"
+			/>
 			<div className="login-usuario">
 				<h2>Acesse sua conta</h2>
 				<form onSubmit={formik.handleSubmit}>
 					<section className="input-login">
 						<img src={EmailLogin} alt="ícone de email" />
-					<input id="email" name="email" value={formik.values.email} onChange={formik.handleChange} type="email" placeholder="Insira seu e-mail" />
+						<input
+							id="email"
+							type="email"
+							placeholder="Insira seu e-mail"
+							value={formik.values.email}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+						/>
 					</section>
+					{formik.touched.email && formik.errors.email ? (
+						<span className="formik-erro">{formik.errors.email}</span>
+					) : null}
 					<section className="input-login">
-                        <img src={Lock} alt="ícone de senha" />
-						<input id="senha" name="senha" value={formik.values.senha} onChange={formik.handleChange} type="password" placeholder="Insira sua senha" />
+						<img src={Lock} alt="ícone de senha" />
+						<input
+							id="senha"
+							type="senha"
+							placeholder="Insira sua senha"
+							value={formik.values.senha}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+						/>
 					</section>
-					<button type="submit" className="botao-login">Login</button>
+					{formik.touched.senha && formik.errors.senha ? (
+						<span className="formik-erro">{formik.errors.senha}</span>
+					) : null}
+					<button type="submit" className="botao-login">
+						Login
+					</button>
 				</form>
 				<hr />
 				<p>Você ainda não é nosso cliente?</p>
